ff074f0b9da5aabc4e03507bd22851ca0d5297d9
Greg Lewicki <glewicki@microsoft.com>
2024-10-02 22:04:40 +0200

    update client script

--- Labs/AutomationUsingWebhooks/Assets/client.ps1
+++ Labs/AutomationUsingWebhooks/Assets/client.ps1
@@ -1,12 +1,11 @@
-﻿Get-AzAutomationAccount -Name aamin001 -ResourceGroupName automationLab-dJhXZrbf | Set-AzAutomationAccount -DisableLocalAuth $false
+﻿
 
 
-$uri = "https://b4a52e31-ef95-4da8-bde3-1e756824dd1d.webhook.dewc.azure-automation.net/webhooks?token=qG6kIAe4%2f3Ci%2bsdrehaW%2bivy2bqWOfAlWd%2fXZ8nj0rE%3d"
-
-$headers = @{"From"="glewicki@microsoft.com"}
+$uri = ""
+$headers = @{"From"="user@contoso.com"}
 
 $myvars  = @(
-    @{AzureResourceGroup="automationLab-dJhXZrbf";Shutdown="false";UamiClientID="8b79fee6-828a-4dfe-b730-2d14d288e51b" }
+    @{AzureResourceGroup="automationLab-dJhXZrbf";Shutdown="true";UamiClientID="8b79fee6-828a-4dfe-b730-2d14d288e51b" }
 )
 
 $body = ConvertTo-Json -InputObject $myvars 
